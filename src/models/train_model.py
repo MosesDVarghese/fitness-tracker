2a2a066e841855a21073da37b04cadad9d932899
amv2 <amv2@hw.ac.uk>
2024-08-15 21:33:55 +0400

    fix train model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -67,22 +67,21 @@ feature_set_4 = list(set(feature_set_3 + freq_features + cluster_features))
 learner = ClassificationAlgorithms()
 
 max_features = 10
-
 selected_features, ordered_features, ordered_scores = learner.forward_selection(
     max_features, X_train, y_train
 )
 
 selected_features = [
+    "pca_1",
     "acc_z_freq_0.0_Hz_ws_14",
     "acc_x_freq_0.0_Hz_ws_14",
-    "acc_y_freq_0.0_Hz_ws_14",
-    "gyr_z_freq_0.714_Hz_ws_14",
-    "gyr_r_freq_1.071_Hz_ws_14",
-    "gyr_z_freq_0.357_Hz_ws_14",
+    "gyr_r_freq_0.0_Hz_ws_14",
+    "acc_r_freq_0.357_Hz_ws_14",
+    "acc_r_temp_mean_ws_5",
+    "acc_z_freq_0.714_Hz_ws_14",
+    "acc_z_freq_1.429_Hz_ws_14",
+    "acc_x_freq_1.071_Hz_ws_14",
     "gyr_x_freq_1.071_Hz_ws_14",
-    "gyr_r_pse",
-    "acc_x_max_freq",
-    "gyr_z_max_freq",
 ]
 
 plt.figure(figsize=(10, 5))
@@ -102,6 +101,7 @@ possible_feature_sets = [
     feature_set_2,
     feature_set_3,
     feature_set_4,
+    selected_features,
 ]
 
 feature_names = [
@@ -109,84 +109,17 @@ feature_names = [
     "Feature Set 2",
     "Feature Set 3",
     "Feature Set 4",
+    "Selectect Features",
 ]
 
 iterations = 1
 score_df = pd.DataFrame()
 
-for i, feature_set in enumerate(possible_feature_sets):
-    print(f"Training with {feature_names[i]}")
-    
-    # Check for missing features in the feature set
-    missing_features = [f for f in feature_set if f not in x_train.columns]
-    
-    if missing_features:
-        print(f"Missing features in {feature_names[i]}: {missing_features}")
-        continue
-    
-    selected_train_X = x_train[feature_set]
-    selected_test_X = x_test[feature_set]
-
-    # Model training and evaluation
-    performance_test_rf = 0
-    for it in range(iterations):
-        print("\tTraining random forest,", it)
-        (
-            class_train_y,
-            class_test_y,
-            class_train_prob_y,
-            class_test_prob_y,
-        ) = learner.random_forest(
-            selected_train_X, y_train, selected_test_X, gridsearch=True
-        )
-        performance_test_rf += accuracy_score(y_test, class_test_y)
-
-    performance_test_rf /= iterations
-
-    # Save results to dataframe
-    new_scores = pd.DataFrame(
-        {
-            "model": ["RF"],
-            "feature_set": feature_names[i],
-            "accuracy": [performance_test_rf],
-        }
-    )
-    score_df = pd.concat([score_df, new_scores])
-
-# Create a grouped bar plot to compare the results
-score_df.sort_values(by="accuracy", ascending=False, inplace=True)
-
-plt.figure(figsize=(10, 6))
-sns.barplot(x="model", y="accuracy", hue="feature_set", data=score_df)
-plt.xlabel("Model")
-plt.ylabel("Accuracy")
-plt.ylim(0.7, 1)
-plt.legend(loc="lower right")
-plt.show()
-
-
-
-iterations = 1
-score_df = pd.DataFrame()
-
-
+# grid search for multiple ML models
 for i, f in zip(range(len(possible_feature_sets)), feature_names):
     print("Feature set:", i)
-    print("Available features:", x_train.columns)
-    print("Requested features:", possible_feature_sets[i])
-    
-    # Check if all requested features are available in the DataFrame columns
-    missing_features = [f for f in possible_feature_sets[i] if f not in x_train.columns]
-    
-    if missing_features:
-        print(f"Missing features: {missing_features}")
-        continue
-    
-    selected_train_X = x_train[possible_feature_sets[i]]
-    selected_test_X = x_test[possible_feature_sets[i]]
-
-    # Rest of your code...
-
+    selected_train_X = X_train[possible_feature_sets[i]]
+    selected_test_X = X_test[possible_feature_sets[i]]
 
     # First run non deterministic classifiers to average their score.
     performance_test_nn = 0
@@ -271,6 +204,7 @@ for i, f in zip(range(len(possible_feature_sets)), feature_names):
     )
     score_df = pd.concat([score_df, new_scores])
 
+
 # --------------------------------------------------------------
 # Create a grouped bar plot to compare the results
 # --------------------------------------------------------------
@@ -290,20 +224,14 @@ plt.show()
 # Select best model and evaluate results
 # --------------------------------------------------------------
 
-missing_features = [f for f in feature_set_4 if f not in x_train.columns]
-
-if missing_features:
-    print(f"Missing features in feature_set_4: {missing_features}")
-else:
-    (class_train_y, class_test_y, class_train_prob_y, class_test_prob_y) = (
-        learner.random_forest(
-            x_train[feature_set_4], y_train, x_test[feature_set_4], gridsearch=True
-        )
-    )
-
-    accuracy = accuracy_score(y_test, class_test_y)
-    print(f"Accuracy with feature_set_4: {accuracy}")
-
+(
+    class_train_y,
+    class_test_y,
+    class_train_prob_y,
+    class_test_prob_y
+) = learner.random_forest(
+    X_train[feature_set_4], y_train, X_test[feature_set_4], gridsearch=True
+)
 
 accuracy = accuracy_score(y_test, class_test_y)
 
@@ -339,113 +267,103 @@ plt.show()
 
 participant_df = df.drop(["set", "category"], axis=1)
 
-x_train = participant_df[participant_df["participant"] != "A"].drop("label", axis=1)
+X_train = participant_df[participant_df["participant"] != "A"].drop("label", axis=1)
 y_train = participant_df[participant_df["participant"] != "A"]["label"]
 
-x_test = participant_df[participant_df["participant"] == "A"].drop("label", axis=1)
+X_test = participant_df[participant_df["participant"] == "A"].drop("label", axis=1)
 y_test = participant_df[participant_df["participant"] == "A"]["label"]
 
-x_train = x_train.drop(["participant"], axis=1)
-x_test = x_test.drop(["participant"], axis=1)
+X_train = X_train.drop(["participant"], axis=1)
+X_test = X_test.drop(["participant"], axis=1)
 
 fig, ax = plt.subplots(figsize=(10, 5))
 df_train["label"].value_counts().plot(
     kind="bar", ax=ax, color="lightblue", label="total"
 )
-
 y_train.value_counts().plot(kind="bar", ax=ax, color="dodgerblue", label="Train")
 y_test.value_counts().plot(kind="bar", ax=ax, color="royalblue", label="Test")
 plt.legend()
 plt.show()
 
+
 # --------------------------------------------------------------
 # Use best model again and evaluate results
 # --------------------------------------------------------------
 
-missing_features = [f for f in feature_set_4 if f not in x_train.columns]
+(
+    class_train_y, 
+    class_test_y, 
+    class_train_prob_y, 
+    class_test_prob_y
+) = learner.random_forest(
+    X_train[feature_set_4], y_train, X_test[feature_set_4], gridsearch=True
+)
 
-if missing_features:
-    print(f"Missing features in feature_set_4: {missing_features}")
-else:
-    (class_train_y, class_test_y, class_train_prob_y, class_test_prob_y) = (
-        learner.random_forest(
-            x_train[feature_set_4], y_train, x_test[feature_set_4], gridsearch=True
-        )
+accuracy = accuracy_score(y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
     )
-
-    accuracy = accuracy_score(y_test, class_test_y)
-    print(f"Accuracy with feature_set_4: {accuracy}")
-
-    classes = class_test_prob_y.columns
-    cm = confusion_matrix(y_test, class_test_y, labels=classes)
-
-    # create confusion matrix for cm
-    plt.figure(figsize=(10, 10))
-    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
-    plt.title("Confusion matrix")
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    thresh = cm.max() / 2.0
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(
-            j,
-            i,
-            format(cm[i, j]),
-            horizontalalignment="center",
-            color="white" if cm[i, j] > thresh else "black",
-        )
-    plt.ylabel("True label")
-    plt.xlabel("Predicted label")
-    plt.grid(False)
-    plt.show()
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
 
 # --------------------------------------------------------------
-# Try a simpler model with the selected features
+# Try a more complex model with the selected features
 # --------------------------------------------------------------
 
-# Check for missing features
-missing_features = [f for f in feature_set_4 if f not in x_train.columns]
+(
+    class_train_y, 
+    class_test_y, 
+    class_train_prob_y, 
+    class_test_prob_y
+) = learner.feedforward_neural_network(
+    X_train[feature_set_4], y_train, X_test[feature_set_4], gridsearch=False
+)
 
-if missing_features:
-    print(f"Missing features in feature_set_4: {missing_features}")
-else:
-    # Train the feedforward neural network
-    (class_train_y, class_test_y, class_train_prob_y, class_test_prob_y) = (
-        learner.feedforward_neural_network(
-            x_train[feature_set_4], y_train, x_test[feature_set_4], gridsearch=False
-        )
+accuracy = accuracy_score(y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
     )
-
-    # Calculate accuracy
-    accuracy = accuracy_score(y_test, class_test_y)
-    print(f"Accuracy with feature_set_4: {accuracy}")
-
-    # Generate confusion matrix
-    classes = class_test_prob_y.columns
-    cm = confusion_matrix(y_test, class_test_y, labels=classes)
-
-    # Plot confusion matrix
-    plt.figure(figsize=(10, 10))
-    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
-    plt.title("Confusion matrix")
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    thresh = cm.max() / 2.0
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(
-            j,
-            i,
-            format(cm[i, j]),
-            horizontalalignment="center",
-            color="white" if cm[i, j] > thresh else "black",
-        )
-    plt.ylabel("True label")
-    plt.xlabel("Predicted label")
-    plt.grid(False)
-    plt.show()
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
